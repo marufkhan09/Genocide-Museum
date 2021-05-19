685afd241e6eb99ba908d3cb89c8a09c7bc2a1ad
Maruf Khan <maruf1609@gmail.com>
2021-05-19 14:01:13 +0600

    Initial Commit

---
+++ Genocide-Museum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Genocide-Museum
+//
+//  Created by Maruf Khan on 19/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
